e9c7ccba6efeeb2f8a3860512a6f831b6c104739
Fabian Thorand <f.thorand@gmail.com>
2021-02-17 19:31:55 +0100

    Implement rudimentary dependency handling and build.rs scripts

--- rust-packages.nix
+++ rust-packages.nix
@@ -5,16 +5,71 @@ let
     version,
     src,
     doCheck ? true,
-    edition ? "2018",
+    features ? [],
+    rustDepends ? [],
+    rustBuildDepends ? [],
+    rustDevDepends ? [],
+    edition ? "2015",
   }: stdenv.mkDerivation {
     inherit name version src doCheck;
 
     phases = [ "unpackPhase" "buildPhase" "checkPhase" "installPhase" ];
 
-    nativeBuildInputs = [rustc];
+    buildInputs = rustDevDepends ++ rustDepends;
+    # TODO: how to doCheck when cross compiling
+    nativeBuildInputs = [rustc] ++ rustBuildDepends;
 
+    RUSTC=rustc + "/bin/rustc";
+
+    crate_features = features;
+    crate_extern = builtins.map (dep: "${dep.name}=${dep}/lib/lib${dep.name}.rlib") rustDepends;
     buildPhase = ''
-      mkdir -p target/nix/lib
+      # rustc flags applicable to all builds
+      rustc_flags=()
+      # rustc flags only applicable to builds that are supposed to be run on the native architecture
+
+      if [[ -f build.rs ]]; then
+        echo "Building build script"
+        rustc \
+          --crate-type bin \
+          --edition ${edition} \
+          --crate-name build_script_build \
+          --out-dir target/nix/build \
+          build.rs
+
+        echo "Running build script"
+        
+        while read -r line; do
+          
+          case "$line" in
+            cargo:rustc-cfg=*)
+              cfg=''${line#*=}
+              rustc_flags+=("--cfg=$cfg")
+              ;;
+            *)
+              echo "Unknown build script flag: $line"
+              exit 1
+          esac
+        done < <(target/nix/build/build_script_build)
+      fi
+
+      for feature in "''${crate_features[@]}"; do
+        rustc_flags+=("--cfg=feature=\"$feature\"")
+      done
+
+      for dep in "''${buildInputs[@]}"; do
+        if [[ ! -z $dep ]] && [[ -d $dep/lib ]]; then
+          rustc_flags+=("-L" "$dep/lib")
+        fi
+      done
+
+      for dep in "''${crate_extern[@]}"; do
+        if [[ ! -z $dep ]]; then
+          rustc_flags+=("--extern" "$dep")
+        fi
+      done
+
+      echo "''${rustc_flags[@]}"
 
       echo "Building library"
 
@@ -23,16 +78,24 @@ let
         --edition ${edition} \
         --crate-name ${name} \
         --out-dir target/nix/lib \
+        "''${rustc_flags[@]}" \
         src/lib.rs
 
       echo "Building tests"
 
-      rustc \
-        --edition ${edition} \
-        --crate-name ${name} \
-        --out-dir target/nix/test \
-        --test \
-        src/lib.rs
+      if (( doCheck )); then
+
+        rustc \
+          --edition ${edition} \
+          --crate-name ${name} \
+          --out-dir target/nix/test \
+          --test \
+          "''${rustc_flags[@]}" \
+          src/lib.rs
+
+        # TODO: rustdoc tests
+
+      fi
 
       echo "Building binaries"
 
@@ -46,6 +109,7 @@ let
           --crate-name $binname \
           --out-dir target/nix/bin \
           --extern ${name}=./target/nix/lib/lib${name}.rlib \
+          "''${rustc_flags[@]}" \
           $bin
       done
 
@@ -61,6 +125,7 @@ let
           --crate-name $exname \
           --out-dir target/nix/examples \
           --extern ${name}=./target/nix/lib/lib${name}.rlib \
+          "''${rustc_flags[@]}" \
           $ex
       done
     '';
@@ -82,14 +147,18 @@ let
     '';
   };
 
-  buildCratesIoCrate = {mkDerivation, fetchCratesIo}: {
+  buildCratesIoCrate = {mkDerivation, fetchCratesIo, lib}: args@{
     name,
     version,
     sha256,
-  }: mkDerivation {
-    inherit name version;
-    src = fetchCratesIo { inherit name version sha256; };
-  };
+    ...
+  }: 
+    let
+      fwdArgs = lib.filterAttrs (name: value: name != "sha256") args;
+    in
+      mkDerivation (fwdArgs // {
+        src = fetchCratesIo { inherit name version sha256; };
+      });
 in
   lib.makeScope pkgs.newScope (self: {
     inherit (pkgs) rustc cargo;
--- test.nix
+++ test.nix
@@ -1,9 +1,38 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
   rust-packages = nixpkgs.callPackage ./rust-packages.nix {};
+  unkHash = "0000000000000000000000000000000000000000000000000000";
 in
-  rust-packages.buildCratesIoCrate {
-    name = "hello";
-    version = "1.0.4";
-    sha256 = "0kgyagy0xpzmb78wyfacnq33q85vndspaj610lhnm3qg1xk788jk";
+  rec {
+    libc = rust-packages.buildCratesIoCrate {
+      name = "libc";
+      version = "0.2.66";
+      sha256 = "0wz5fdpjpj8qp7wx7gq9rqckd2bdv7hcm5631hq03amxy5ikhi3l";
+      features = ["std"];
+    };
+
+    rand_core = rust-packages.buildCratesIoCrate {
+      name = "rand_core";
+      version = "0.5.1";
+      sha256 = "19qfnh77bzz0x2gfsk91h0gygy0z1s5l3yyc2j91gmprq60d6s3r";
+      features = [];
+    };
+
+    rand_hc = rust-packages.buildCratesIoCrate {
+      name = "rand_hc";
+      version = "0.2.0";
+      edition = "2018";
+      sha256 = "0592q9kqcna9aiyzy6vp3fadxkkbpfkmi2cnkv48zhybr0v2yf01";
+      features = [];
+      rustDepends = [rand_core];
+    };
+
+    # memchr = rust-packages.buildCratesIoCrate {
+    #   name = "memchr";
+    #   version = "2.3.0";
+    #   sha256 = "0sf6y998273290a6zxx5c28ps196vzz3q9yd0v5jxflpm3bp52f1";
+    #   features = ["libc"];
+    #   rustDepends = [libc];
+    #   doCheck = false;
+    # };
   }
